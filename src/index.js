ff559f29752f20861ee1be369cd2e0df75f62319
Slava Torchik <torhik_slava@mail.ru>
2018-09-16 17:03:14 +0300

    sorter/solution: 20 out of 25 tests passed

--- src/index.js
+++ src/index.js
@@ -1,26 +1,43 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.arr = [];
   }
 
   add(element) {
     // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.arr[index];
   }
 
   get length() {
     // your implementation
+    return this.arr.length;
   }
 
   toArray() {
     // your implementation
+    return Array.from(this.arr);
   }
 
   sort(indices) {
     // your implementation
+    var partOfArr = [];
+    for (var i=0; i<indices.length; i++){
+      partOfArr[i]=this.arr[indices[i]];
+    }
+    partOfArr.sort(function(a,b){return a-b;});
+    // строка 36 для того, чтобы при входящих индексах не по возрастанию, после
+    // сортировки части массивва вставить их на правильные места (по порядку) 
+    indices.sort();
+    for (var i=0; i<indices.length; i++){
+    this.arr[indices[i]]=partOfArr[i]; 
+    }
+    return this.arr;
   }
 
   setComparator(compareFunction) {
